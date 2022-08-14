178dec6a8e66b1ad76e43d8476c757ae9f406e9d
r4deadv <mihovilovic.rade@gmail.com>
2022-08-14 14:48:16 +0200

    module 5 added

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,12 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -18,6 +24,7 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
